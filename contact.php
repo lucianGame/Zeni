cc1f97344b8fd14c0c4e1bd05daae1a44ef86f20
Lucian <147219578+lucianGame@users.noreply.github.com>
2026-01-19 20:32:12 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "lucianmeisler@gmail.com";
+    $email_to = "zenithhydensoffer@alum.calarts.edu";
     $email_subject = "New form submissions";
 
     function problem($error)
